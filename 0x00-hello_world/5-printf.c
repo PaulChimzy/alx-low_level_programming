5d4036b3261148d3a03b127d0307761e295169a0
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-16 13:35:00 +0200

    Create 5-printf.c
    Using printf to print strings to the screen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints some string or characters to the screen
+ * Return: 0 if everything runs fine
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
