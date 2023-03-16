70dc95ab362f27e1fd49ec93aa658eca504fd9f1
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-16 13:23:32 +0200

    Update 4-puts.c
    Oh Betty Coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main(void) /* Description: prints a series of string to the screen */
+/** 
+ * main - prints a series of string to the screen 
+ */
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
