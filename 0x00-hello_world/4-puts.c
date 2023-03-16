688e66281f9843d22a1867ff73c635781f21ccd6
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-16 13:06:13 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* prints a series of string to the screen */
+/* 
+ * Description: prints a series of string to the screen 
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
