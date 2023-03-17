7b9d3dc3b40b503c0f3e4045224fd89500da4157
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-18 00:55:36 +0200

    Create 1-last_digit.c
    Checking the status of a number based on a bunch of if..else

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Checks if a number is either positive or negative
+ * Return: returns the verdict of the number
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	
+	int last_d = n % 10;
+	if (last_d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, last_d);
+	}
+
+	else if (last_d == 0)
+	{
+		printf("Last digit of %d is 0 and is 0", n);
+	}
+
+	else if (last_d < 6 && last_d > 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_d);
+	}
+	return (0);
+}
