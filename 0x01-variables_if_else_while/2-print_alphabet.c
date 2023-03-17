107c201b3a4323466bc5d3ecf66fe4ed8fb1a614
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-18 01:14:10 +0200

    Create 2-print_alphabet.c
    Now we are printing lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ * Return: returns 0 if everything works fine
+ */
+
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
