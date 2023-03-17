5899c6ad41178389793d67836674aac745a7338c
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-18 01:29:27 +0200

    Create 5-print_numbers.c
    Printing numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers in base 10 begining from 0
+ * Return: 0 if everything works fine
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf('%d',i);
+	}
+
+	return (0);
+}
