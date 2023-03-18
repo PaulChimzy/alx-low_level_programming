b3952248625f9d5ad9c221072e4ee36a222852f3
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-18 02:32:23 +0200

    Create 9-print_comb.c
    Printing out numbers decently

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers present in base16
+ * Return: returns 0 if everything works fine
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if(i != 9)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(i + '0');
+		}
+	}
+
+	return (0);
+}
