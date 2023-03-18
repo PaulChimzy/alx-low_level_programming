e2ee71da3851455863183e00a3a013fcd812cfed
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-18 03:12:02 +0200

    Create 101-print_comb4.c
    Now we are printing three-digits combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Prints differnt three digit numbers possible combinations
+ * Return: returns 0 if everything works out fine
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 1; j < 9; j++)
+		{
+			for (k = 2; k < 10; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i == 7 && j == 8 && k == 9)
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
