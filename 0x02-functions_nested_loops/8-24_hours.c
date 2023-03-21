979be00490fb8e476e603d476273e47035d570d1
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-22 01:46:03 +0200

    Create 8-24_hours.c
    Printing hours of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints the hours in a day
+ * Return: returns 0 if everything works fine
+ */
+
+void jack_bauer(void)
+{
+	int one;
+	int two;
+	int three;
+	int four;
+
+	for (one = 0; one < 3; one++)
+	{
+		_putchar('0' + one);
+		for (two = 0; two < 10; two++)
+		{
+			_putchar('0' + tw0);
+			_putchar(':');
+			for (three = 0; three < 7; three++)
+			{
+				_putchar('0' + three);
+				for (four = 0; four < 10; four++)
+				{
+					_putchar('0' + four);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
