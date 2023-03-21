3c2763e54ace0f3349a56ede7080b2e12945748e
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-22 01:09:38 +0200

    Create 6-abs.c
    Absolute numbers are less complicated

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - determines the sign of a number
+ * @n: an integer for testing
+ * Return: returns 1 if true else returns 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		return (n*-1);
+	}
+}
