c1e3d0c8c8f250778e721108b2a44c52b01bcb8e
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-22 02:12:35 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,19 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar('0' + (i * j));
-			_putchar(',');
-			_putchar(' ');
+			if ((i * j) > 9)
+			{
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + (i * j));
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
