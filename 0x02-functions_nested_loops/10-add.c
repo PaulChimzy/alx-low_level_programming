473d134c4e3d90fa3eca231827eb4e7c26c51004
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-22 02:47:59 +0200

    Create 10-add.c
    Adding two integers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ * Return: returns the sum of the two integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
