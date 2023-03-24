2ae2870d4bf075f6d6f0511086d27ddf4f0e6f0b
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-24 22:52:38 +0200

    Create 9-fizz_buzz.c
    Passing the FizzBuzz Interview

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Passes the FizzBuzz test check the description on Google
+ * Return: returns 0 if everything works fine
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
