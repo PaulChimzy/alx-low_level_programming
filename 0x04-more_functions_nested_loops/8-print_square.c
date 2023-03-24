c927ce412967d82204b19a2082f14e1672d9d237
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-24 22:41:30 +0200

    Create 8-print_square.c
    Printing squares with the # symbol

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square of  a given size using #
+ * @n: The number of times # should be printed to form a square
+ * Return: returns 0 if everything works fine
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
