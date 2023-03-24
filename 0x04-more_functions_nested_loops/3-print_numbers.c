d250b5fcc3f3557d020e81ef8b4a99a510ab01ca
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-24 21:15:45 +0200

    Create 3-print_numbers.c
    Printing digits from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 through to 9
+ * Return: returns 0 if everything works fine
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	putchar('\n');
+}
