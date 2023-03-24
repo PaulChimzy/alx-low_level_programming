e678d71961899c849814ceb3ca1f9b5e3fb10551
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-24 22:12:13 +0200

    Create 7-print_diagonal.c
    Printing diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - prints blackslashes to form a diagonal
+ * @n: The number of times \ should be printed
+ * Return: returns 0 if everything works fine
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\');
+		}
+		_putchar('\n');
+	}
+}
