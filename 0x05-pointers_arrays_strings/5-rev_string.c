29bcae137f2f21ac836690b70fb0c263071a11a5
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-27 22:41:51 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,17 +10,23 @@
 void rev_string(char *s)
 {
 	int count;
+	int i;
 
 	count = 0;
 	while (*(s + count) != '\0')
 	{
 		count++;
 	}
+	char temp[count];
+
+	for (i = 0; i < count; i++)
+	{
+		temp[i] = *(s + count);
+	}
 	count--;
 	while (count >= 0)
 	{
-		putchar(*(s + count));
+		*(s + count) = temp[count]
 		count--;
 	}
-	putchar('\n');
 }
