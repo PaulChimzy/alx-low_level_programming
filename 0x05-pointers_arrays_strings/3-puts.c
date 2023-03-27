6cd9be827ce32ad6d0c8ec7597c0ad4b7780882b
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-27 22:05:30 +0200

    Create 3-puts.c
    Writing to the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - writes a string to the stdout
+ * @str: pointer to the string (array of characters)
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		putchar(*(s + count))
+		count++;
+	}
+	putchar('\n');
+}
