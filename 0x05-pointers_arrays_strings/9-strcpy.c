e76545e4fb085f444acaab8ece938300b65cb55b
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-28 01:07:12 +0200

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * _strcpy - copies a string to a buffer
+ * @dest: Where the strings should be copied into
+ * @src: Where the strings should be copied from
+ * Return: a pointer to the destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+
+	count = 0;
+	while (*(src + count) != '\0')
+	{
+		*(dest + count) = *(src + count)
+		count++;
+	}
+	*(dest + count) = *(src + count)
+	return (dest);
+}
