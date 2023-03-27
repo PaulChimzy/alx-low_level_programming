8171cef379631f670c894742a80b419b583e7b11
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-27 22:26:50 +0200

    Create 5-rev_string.c
    Reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string and prints to the stout
+ * @s: pointer to the string (array of characters)
+ * Return: the length of the string
+ */
+
+void rev_string(char *s);
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	count--;
+	while (count >= 0)
+	{
+		putchar(*(s + count));
+		count--;
+	}
+	putchar('\n');
+}
