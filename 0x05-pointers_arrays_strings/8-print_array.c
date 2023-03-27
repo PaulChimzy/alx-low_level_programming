fc69b023eaaa57fa15fbba9a772faeb71b3827c1
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-28 00:37:23 +0200

    Create 8-print_array.c
    Printing integers in a formatted manner

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints elements from an array
+ * @a: pointer to the string (array of characters)
+ * @n: number of characters to be printed
+ * Return: the length of the string
+ */
+
+void print_array(int *a, int n)
+{
+	int count;
+
+	count = 0;
+	while (*(a + count) != '\0' && count < n)
+	{
+		printf("%d", *(a + count));
+		if (count != n - 1)
+		{
+			printf(" ,");
+			count++;
+		}
+		count++;
+	}
+	printf("\n");
+}
