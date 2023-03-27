e06b76fac0e13ee1c42266d8a8f5f8f4bbaefc10
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-28 00:22:35 +0200

    Create 7-puts_half.c
    Printing half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half -prints the last half of a string
+ * @str: pointer to the string (array of characters)
+ * Return: the length of the string
+ */
+
+void puts_half(char *str)
+{
+	int count;
+	int start;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	count--;
+	start = count / 2;
+	if (count % 2 != 0)
+	{
+		start = (count + 1) / 2;
+	}
+	while (start != count + 1)
+	{
+		putchar(*(s + start));
+		start++;
+	}
+	putchar('\n');
+}
