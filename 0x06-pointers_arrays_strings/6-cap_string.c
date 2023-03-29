811d875331286c4799dd2ee80f6a85354a7425d7
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-30 01:04:44 +0200

    Create 6-cap_string.c
    Converting lowercase to uppercase on special conditions

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - converts all lowercase characters to uppercase based on some delimeters
+ * @s: pointer to the string to be converted to uppercase
+ * Return: a pointer to the converted string
+ */
+
+char *cap_string(char *s)
+{
+	int count;
+
+	count = 0;
+	while(*(s + count) != '\0')
+	{
+		if (count == 0)
+		{
+			if (*(s + count) > 96 && *(s + count) < 123)
+			{
+				*(s + count) = *(s + count) - 32;
+			}
+			count++;
+		}
+		else
+		{
+			if ((*(s + count - 1) > 31 && *(s + count - 1) < 35) || *(s + count - 1) == '\t' || *(s + count - 1) == '\n' || *(s + count - 1) == ',' || *(s + count - 1) == ';' || *(s + count - 1) == '.' || *(s + count - 1) == '?' || *(s + count - 1) == '(' || *(s + count - 1) == ')' || *(s + count - 1) == '{' || *(s + count - 1) == '}')
+			{
+				if (*(s + count) > 96 && *(s + count) < 123)
+				{
+					*(s + count) = *(s + count) - 32;
+				}
+			}
+			count++;
+		}
+	}
+	return (s);
+}
