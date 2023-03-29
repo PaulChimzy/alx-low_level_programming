96a4729515af5c0654c2cf5233432de191322114
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-03-30 00:14:10 +0200

    Create 5-string_toupper.c
    converting lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - converts all lowercase characters to uppercase
+ * @s: pointer to the string tobe converted to uppercase
+ * Return: a pointer to the converted string
+ */
+
+char *string_toupper(char *s)
+{
+	int count;
+
+	count = 0;
+	while(*(s + count) != '\0')
+	{
+		if (*(s + count) > 96 && *(s + count) < 123)
+		{
+			*(s + count) = *(s + count) - 32;
+			count++;
+			continue;
+		}
+		count++;
+	}
+	return (s);
+}
