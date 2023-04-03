06290d711b7020f974acf1511f637a4e8c6a7c76
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-04-03 20:42:26 +0200

    Update 5-strstr.c
    Find a sub-string in a string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,4 +7,39 @@
  * @needle: character to llok for in the string
  * Return: pointer to the character in the string if ound else pointer to NULL
  */
+char *_strstr(char *haystack, char *needle)
+{
+	int count;
+	int counter;
+	int counter2;
+	char *character;
 
+	count = 0;
+	counter = 0;
+	counter2 = 0;
+	character = NULL;
+	while (*(haystack + count) != '\0')
+	{
+		counter = 0;
+		if (*(needle + counter) == *(haystack + count))
+		{
+			counter2 = count
+			while (*(needle + counter) != '\0')
+			{
+				if (*(needle + counter) != *(haystack + counter2))
+				{
+					break;
+				}
+				counter++;
+				counter2++;
+				if (*(needle + counter) == '\0')
+				{
+					character = (s + count);
+					return (character);
+				}
+			}
+		}
+		count++;
+	}
+	return (character);
+}
