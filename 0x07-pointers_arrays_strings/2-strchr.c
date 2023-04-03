ba0a9620828428fa1e109aa8559ef74a817c0e4f
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-04-03 17:39:19 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,12 @@ char *_strchr(char *s, char c)
 	char **character;
 
 	count = 0;
-	**character = NULL;
+	*character = NULL;
 	while (*(s + count) != '\0')
 	{
 		if (*(s + count) == c)
 		{
-			character = &(s + count);
+			*character = &(s + count);
 			break;
 		}
 		count++;
