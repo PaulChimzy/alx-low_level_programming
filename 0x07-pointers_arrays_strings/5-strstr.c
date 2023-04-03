fc5259b6eb672966acf4f55917a2e68a47c1b336
PaulChimzy <63836558+PaulChimzy@users.noreply.github.com>
2023-04-03 20:24:56 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurence of character in a string
+ * @haystack: pointer to the string to be searched
+ * @needle: character to llok for in the string
+ * Return: pointer to the character in the string if ound else pointer to NULL
+ */
+
