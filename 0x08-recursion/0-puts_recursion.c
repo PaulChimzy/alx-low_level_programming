ac970c27b409bfb213099e155d235ef4585bb61b
PaulChimzy <paulchimzy@gmail.com>
2023-04-05 03:26:28 -0700

    Printing a string using _putschar recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints a string to the stdout recursively
+ * @s: pointer to the string to be printed out
+ */
+void _puts_recursion(char *s)
+{
+	int count;
+
+	count = 0;
+	if (*(s + count) != '\0')
+	{
+		_putschar(*(s + count));
+		_puts_recursion((s + count + 1));
+	}
+	else
+	{
+		_putschar('\n');
+	}
+}
