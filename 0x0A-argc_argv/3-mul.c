7a62bbf7f3cde83b03c5fc86b26f8db86b452cbf
PaulChimzy <paulchimzy@gmail.com>
2023-04-12 07:25:16 -0700

    Multiplying two numbers from the argument line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints the multiplication passed as an argumnet to the main function
+ * @argc: the number of arguments to the function main
+ * @argv: pointer to the array of argumnts to the program
+ * Return: 0 if everything works fine 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int f_digit;
+	int s_digit;
+
+	if (argc == 3)
+	{
+		if (sscanf(argv[1], "%d", &f_digit) && sscanf(argv[2], "%d", &s_digit))
+		{
+			printf("%d\n", (f_digit * s_digit));
+			return (0);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
