e1567aab58aaa8ac503460976a338dc8a0238770
PaulChimzy <paulchimzy@gmail.com>
2023-04-13 13:24:29 -0700

    Allocating a memory and initializing all to 0

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocates dynamic memory and initializes to 0
+ * @nmemb: number of item type that should be allocated
+ * @size: the size of each type
+ * Return: pointer to the memory allocated
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	arr = NULL;
+	if (nmemb == 0 || size = 0)
+	{
+		return (arr);
+	}
+	else
+	{
+		arr = malloc(size * nmemb);
+		if (arr == NULL)
+		{
+			return (arr);
+		}
+		else
+		{
+			for (i = 0; i < (size * nmemb); i++)
+			{
+				arr[i] = 0;
+			}
+		}
+		return (arr);
+	}
+}
