874ca804f27c6419df23c526daeb49a6b24278a1
PaulChimzy <paulchimzy@gmail.com>
2023-04-17 14:30:27 -0700

    Defining a structure  of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - a structure for the dog class
+ * @name: name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
