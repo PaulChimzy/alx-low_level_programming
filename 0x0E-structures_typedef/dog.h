e8d66117bb6e4a02289cdefb090dd6f300c8d26b
PaulChimzy <paulchimzy@gmail.com>
2023-04-17 15:23:22 -0700

    Printing out the variables of struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: The age of the dog
  * @owner: The name of the owner of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
