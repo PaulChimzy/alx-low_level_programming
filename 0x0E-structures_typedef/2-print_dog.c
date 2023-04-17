e8ec3f7a0a581f216416c28ed423f583cf858fb0
PaulChimzy <paulchimzy@gmail.com>
2023-04-17 14:58:18 -0700

    Printing out the variables of struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - prints the attributes of the struct dog
+ * @d: ponter to the declared dog structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("nothing")
+	}
+	else
+	{
+		if (d->name == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%s\n", d->name);
+		}
+
+		if (d->age == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%d\n", d->age);
+		}
+
+		if (d->owner == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%s\n", d->owner);
+		}
+	}
+}
