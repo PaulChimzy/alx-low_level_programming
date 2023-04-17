4b9a0237773dc05ce9dec4f989d5993d66b64fd0
PaulChimzy <paulchimzy@gmail.com>
2023-04-17 13:09:10 -0700

    Printing the name of file compiled using macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it is compiling
+ * Return - 0 if everything works fine
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
