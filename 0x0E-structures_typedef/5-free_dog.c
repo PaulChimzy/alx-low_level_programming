c7b608a8f80ac00e39f8700a37bb89bca15893e4
PaulChimzy <paulchimzy@gmail.com>
2023-04-17 17:37:07 -0700

    copying from one space to another

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - frees up space of a defined structure of a dog
+ * @d: pointer to the initialized dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
