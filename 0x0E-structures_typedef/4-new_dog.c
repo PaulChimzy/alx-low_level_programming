d266259cc701776a19f43a23bd4f6a5c0d5544eb
PaulChimzy <paulchimzy@gmail.com>
2023-04-17 17:57:17 -0700

    copying from one space to another

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -48,12 +48,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog_;
 
-	dog_ = NULL;
+	dog_ = malloc(sizeof(dog_t));
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (dog_);
 
-	dog_->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	dog_->name = malloc(sizeof(char) * (_strlen(name)));
 	dog_->age = age;
 
 	if (dog_->name == NULL)
@@ -61,13 +61,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog_);
 		return (NULL);
 	}
-	dog_->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	dog_->owner = malloc(sizeof(char) * (_strlen(owner)));
 	if (dog_->owner == NULL)
 	{
 		free(dog_->name);
 		return (NULL);
 	}
-	dog_->age = age;
 	dog_->name = _strcpy(dog_->name, name);
 	dog_->owner = _strcpy(dog_->owner, owner);
 
