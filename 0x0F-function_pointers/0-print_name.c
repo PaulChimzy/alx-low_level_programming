b6667c106392f93e83ce967f26c6b89b56f31a8e
PaulChimzy <paulchimzy@gmail.com>
2023-04-19 13:10:04 -0700

    Calling a function using a pointer to the function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - prints to the console the given name
+ * @name: pointer to the name that needs to be printed
+ * @f: pointer to the function that prints the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
