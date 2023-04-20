3b4b580c75614f595a5d3521fb326ffa198ecea0
PaulChimzy <paulchimzy@gmail.com>
2023-04-20 14:58:34 -0700

    Some bugs fixing

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,42 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
-/**
- * switchup - checks the correct value and prints
- * @holder: a character to be checked
- * @argm: a va_list variable
- */
-void switchup(char holder, va_list argm)
-{
-	char *str;
-
-	switch (holder)
-	{
-		case 'c':
-			printf("%c", va_arg(argm, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(argm, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(argm, double));
-			break;
-		case 's':
-			str = va_arg(argm, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-			{
-				printf("%s", str);
-			}
-			break;
-		default:
-			break;
-	}
-}
-
-
 /**
  * print_all - prints all the values in an argument using specified format
  * @format: list of format that should be used for printing
@@ -44,29 +8,37 @@ void switchup(char holder, va_list argm)
 void print_all(const char * const format, ...)
 {
 	va_list argm;
-	int i;
-	char holder;
+	int i = 0;
+	char *sep = ", ", *str;
 
 	va_start(argm, format);
-	i = 0;
-	holder = 'a';
-
 	while (*(format + i) != '\0')
 	{
-		holder = *(format + i);
-		switchup(holder, argm);
-		i++;
-		if (*(format + i) != '\0' && (*(format + i - 1) == 'c' ||
-		*(format + i - 1) == 'i' || *(format + i - 1) == 's'
-		|| *(format + i - 1) == 'f'))
+		if (*(format + i + 1) == '\0')
+			sep = "";
+		switch (*(format + i))
 		{
-			printf(", ");
+			case 'c':
+				printf("%c%s", va_arg(argm, int), sep);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(argm, int), sep);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(argm, double), sep);
+				break;
+			case 's':
+				str = va_arg(argm, char *);
+				if (str == NULL)
+					printf("(nil)%s", sep);
+				else
+					printf("%s%s", str, sep);
+				break;
+			default:
+				break;
 		}
+		i++;
 	}
 	printf("\n");
 	va_end(argm);
 }
-
-
-
-
