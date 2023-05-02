79bc9313e6492f1cc02b7c5f55de77732f5dc490
Chima Paul <paulchimzy@gmail.com>
2023-05-02 02:26:50 +0200

    Freeing up linked list after

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees up memory consumed by a linked list 
+ * and sets the head to null
+ * @head: pointer to a pointer of the linked list
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nextnode;
+	listint_t *tempnode;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		nextnode = *head;
+		while (nextnode != NULL)
+		{
+			if (nextnode->next == NULL)
+			{
+				free(nextnode);
+			}
+			else
+			{
+				tempnode = nextnode->next->next;
+				free(nextnode->next);
+				nextnode = tempnode;
+				free(tempnode);
+			}
+		}
+		*head = NULL;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint - add a node at the beginning of the new linked list
+ * @head: pointer to the pointer if the head node
+ * @n: value to be stored in the new node added
+ * Return: pointer to the new node added
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	if (*head == NULL)
+	{
+		*head = malloc(sizeof(listint_t));
+		if (*head == NULL)
+			return (NULL);
+		(*head)->n = n;
+		(*head)->next = NULL;
+	}
+	else
+	{
+		newnode = malloc(sizeof(listint_t));
+		if (newnode == NULL)
+			return (NULL);
+		newnode->n = n;
+		newnode->next = *head;
+		*head = newnode;
+	}
+	return (*head);
+}
