88d3080b89185d9b1572f6ff7cd36801449547ad
Chima Paul <paulchimzy@gmail.com>
2023-05-03 02:22:06 +0200

    Adding a node at an index in a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * insert_nodeint_at_index - insert a node at a particular index
+ * delete_nodeint_at_index - delete a node at a particular index
  * @head: pointer to the pointer of the head of node
- * @idx: the index at which the node should be inserted
- * @n: data that should be added in this node
+ * @index: the index at which the node should be inserted
  * Return: pointer to the new node
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *newnode;
 	listint_t *nextnode;
 	unsigned int counter = 0;
 
-	if (head == NULL || idx < 0)
+	if (head == NULL)
 	{
-		return (NULL);
+		return (-1);
 	}
 	else
 	{
@@ -26,7 +25,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (counter == (idx - 1))
 			{
 				newnode = malloc(sizeof(listint_t));
-				newnode->n = n;
 				newnode->next = nextnode->next;
 				nextnode->next = newnode;
 				return (newnode);
