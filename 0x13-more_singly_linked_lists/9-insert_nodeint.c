f7b0eb3636b429e78a74668747d6488d670ac432
Chima Paul <paulchimzy@gmail.com>
2023-05-03 03:39:08 +0200

    Deleting from a particular index in a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *nextnode;
 	unsigned int counter = 0;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else if (*head == NULL)
 	{
 		return (NULL);
 	}
