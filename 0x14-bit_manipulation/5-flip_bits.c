9c4937c5596bf53af0d9e6b1b5561c2a69ef7a5b
Chima Paul <paulchimzy@gmail.com>
2023-05-06 00:15:04 +0200

    Flipping bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * flip_bits - flips a bit
+ * @n: number from which a bit is to be cleared
+ * @index: the index of the bit to be cleared
+ * Return: 1 if everything works fine and 0 otherwise
+ */
+int flip_bits(unsigned long int n, unsigned int index)
+{
+	unsigned long int manipulate = n ^ index;
+	unsigned long int bits = 0;
+	
+	while (manipulate > 0)
+	{
+		bits += (manipulate & 1);
+		manipulate >>= 1;
+	}
+	return (bits);
+}
