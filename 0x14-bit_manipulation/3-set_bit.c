66aeeac2ac57e9da90375ee3868da107bc37645d
Chima Paul <paulchimzy@gmail.com>
2023-05-05 02:51:10 +0200

    Set the bit of a number

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * set_bit - flips a bit at an index
+ * @n: number from which a bit is to be set
+ * @index: the index of the bit to be set
+ * Return: 1 if everything works fine and 0 otherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	*n = *n ^ (1 << index);
+	return (1);
+}
