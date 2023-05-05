329ab4cb8eb70c3a03865abf084848ec947bab24
Chima Paul <paulchimzy@gmail.com>
2023-05-05 02:36:59 +0200

    Gets the bit at an index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <math.h>
+/**
+ * print_binary - prints the binary form of a decimal number
+ * @n: decimal number to be converted to decimal
+ * @index: the index of the bit to be retrieved
+ * Return: the length of the string
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	if (n & (1 << index) == 0)
+		return (0);
+	return (1);
+}
