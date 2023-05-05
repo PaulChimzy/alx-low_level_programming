a31894e1271e80898bdf89478fba8235db1b9f98
Chima Paul <paulchimzy@gmail.com>
2023-05-05 03:23:36 +0200

    Set the bit of a number

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * set_bit - sets a bit to 0 at an index
- * @n: number from which a bit is to be set
- * @index: the index of the bit to be set
+ * clear_bit - clears a bit to 0 at an index
+ * @n: number from which a bit is to be cleared
+ * @index: the index of the bit to be cleared
  * Return: 1 if everything works fine and 0 otherwise
  */
 int clear_bit(unsigned long int *n, unsigned int index)
